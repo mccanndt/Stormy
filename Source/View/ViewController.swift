60f485be0cc3923df847fcb7e0017b6abebcaa09
David McCann <davidmccann@Davids-Mac.local>
2019-01-27 07:40:14 -0800

    First commit

---
+++ Source/View/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by David McCann.
+//  Copyright © David McCann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
